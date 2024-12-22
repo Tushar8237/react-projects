29a86c58750311ce7491e01341e301cf0365d463
Tushar Suryawanshi <tushar.823737@gmail.com>
2024-12-22 16:51:37 +0530

    add responsive layout for project

--- responsive-layout/src/components/Footer.jsx
+++ responsive-layout/src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './AllComponent.css'
+
 
 function Footer() {
   return (
--- responsive-layout/src/components/Header.jsx
+++ responsive-layout/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './AllComponent.css'
 
 function Header() {
   return (
