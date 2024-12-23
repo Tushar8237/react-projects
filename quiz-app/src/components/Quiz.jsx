f62cc93f1144a2797fe0e7ebd1afcccdf8d50f13
Tushar Suryawanshi <tushar.823737@gmail.com>
2024-12-23 13:23:50 +0530

    add functionality for project

--- quiz-app/src/components/Quiz.jsx
+++ quiz-app/src/components/Quiz.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Quiz() {
+function Quiz({ quizData, finishQuiz }) {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [useAnswer, setUseAnswer] = useState("");
+
+  const handleAnswer = () => {
+    if (
+      useAnswer.toLocaleLowerCase() ===
+      quizData[currentQuestion].correctAnswer.toLocaleLowerCase()
+    ) {
+      setScore(score + 1);
+    }
+    setUseAnswer("");
+    if (currentQuestion < quizData.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    } else {
+      finishQuiz(score + 1);
+    }
+  };
   return (
     <div>
-      Quiz component
+      <h2>Take Quiz</h2>
+      <p>{quizData[currentQuestion].question}</p>
+      <input
+        type="text"
+        placeholder="Your Answer"
+        value={useAnswer}
+        onChange={(e) => setUseAnswer(e.target.value)}
+      />
+      <button onClick={handleAnswer}>Next</button>
     </div>
-  )
+  );
 }
 
-export default Quiz
+export default Quiz;
--- quiz-app/src/components/QuizForm.jsx
+++ quiz-app/src/components/QuizForm.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function QuizRorm() {
+function QuizForm({ setQuizData }) {
+  const [question, setQuestion] = useState("");
+  const [correctAnswer, setCorrectAnswer] = useState('');
+  const [quizList, setQuizList] = useState([]);
+
+  const handleAddQuestion = () => {
+    if (question && correctAnswer) {
+      const newQuestion = { question, correctAnswer };
+      setQuizList([...quizList, newQuestion]);
+      setQuestion("");
+      setCorrectAnswer("");
+    }
+  };
+
+  const handleSubmit = () => {
+    setQuizData(quizList);
+  };
+
   return (
     <div>
-      Quiz form component
+      <h1>Create Quiz</h1>
+      <input
+        type="text"
+        placeholder="Question"
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+      />
+
+      <input
+        type="text"
+        placeholder="Correct Answer"
+        value={correctAnswer}
+        onChange={(e) => setCorrectAnswer(e.target.value)}
+      />
+
+      <button onClick={handleAddQuestion}>Add Question</button>
+      <button onClick={handleSubmit}>SubmitQuestion</button>
     </div>
-  )
+  );
 }
 
-export default QuizRorm
+export default QuizForm;
